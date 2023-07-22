1a11e82d2d08dd62e964c1d8e23424fd7d04f055
somnath11som <somnathpoddar615@email.com>
2023-07-22 23:08:37 +0530

    DELETE ROUTE CREATE

--- routes/carRoute.js
+++ routes/carRoute.js
@@ -12,7 +12,9 @@ router.post("/add-car", adminVerify, adminAddCar);
 router.post("/get-all-car", adminVerify, adminGetAllCar);
 router.post("/edit-car/:id", adminVerify, editCar);
 router.post("/delete-car/:id", adminVerify, deleteCar);
+
 router.put("/edit-car/:id", adminVerify, editCar);
 router.patch("/edit-car/:id", adminVerify, editCar);
+router.delete("/delete-car/:id", adminVerify, deleteCar);
 
 module.exports = router;
